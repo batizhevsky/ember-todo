05b192e7330e3682dd8cc5f7033fbe2742198d74
Leonid Batizhevsky <the.leonko@gmail.com>
2014-02-19 21:25:05 +0400

    change adapter to localstore and final guide

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -35,6 +35,16 @@ Todos.TodosController = Ember.ArrayController.extend({
 
 	completed: function() {
 		return this.filterBy('isCompleted', true).get('length');
+	}.property('@each.isCompleted'),
+
+	allAreDone: function(key, value) {
+		if (value === undefined) {
+			return !!this.get('length') && this.everyProperty('isCompleted', true);
+		} else {
+			this.setEach('isCompleted', value);
+			this.invoke('save');
+			return value;
+		}
 	}.property('@each.isCompleted')
 
 });
